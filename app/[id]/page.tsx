f18207e9740323e71fd36513de3443a1c831f101
유병우 <conner903@naver.com>
2025-06-02 10:44:13 +0900

    feat: add content page

---
+++ app/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { getPostContent } from "@/src/features/content/api";
+import { MarkdownView } from "@/src/features/content/ui/MarkdownView";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const postFileName = id.concat(".md");
+  const postContent = await getPostContent(postFileName);
+
+  return {
+    title: postContent.title,
+    description: postContent.summary,
+  };
+}
+
+export default async function ContentPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const postFileName = id.concat(".md");
+  const postContent = await getPostContent(postFileName);
+
+  return (
+    <main>
+      <article>
+        <h1 className="text-4xl font-bold">{postContent.title}</h1>
+        <p className="mt-2 mb-8 text-xs text-gray-500">{postContent.date}</p>
+        <MarkdownView markdownContent={postContent.content} />
+      </article>
+    </main>
+  );
+}
